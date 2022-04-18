3c36a1a390e35a1a20d6fd8f1d73e9bed9e2232b
prrThr <arthur.op9@gmail.com>
2022-04-18 17:28:42 -0300

    Campo Aleatorio + dificuldade

--- projeto.py
+++ projeto.py
@@ -1,15 +1,49 @@
 import random
 
-m = []
+def campoAleatorio(tamanho):
+    for l in range(tamanho):
+        linha = []
+        for c in range(tamanho):
+            num = random.randint(-1, 0)
+            linha.append(num)
+        matriz.append(linha)
 
-for i in range(5):
-    linha = []
-    for j in range(5):
-        num = random.randint(0, 9)
-        linha.append(num)
-    m.append(linha)
+def mostrarMatriz(tamanho):
+    for i in range(tamanho):
+        for j in range(tamanho):
+            print(f'{matriz[i][j]:^3}', ' ', end='')
+        print('')       
 
-for i in range(5):
-    for j in range(5):
-        print(f'[{m[i][j]}]', end=' ')
-    print()
+def dificuldade():
+    print('=====================================')
+    print('ESCOLHA A DIFICULDADE: ')
+    print('1 - Facil')
+    print('2 - Medio')
+    print('3 - Dificil')
+    existe = False 
+    while existe == False:
+        escolhaDificuldade = int(input())
+        if escolhaDificuldade != 1 and escolhaDificuldade != 2 and escolhaDificuldade != 3:
+            existe = False
+        else:   
+            existe = True
+
+    if escolhaDificuldade == 1:
+        return 4
+    elif escolhaDificuldade == 2:
+        return 5
+    else:
+        return 15
+
+
+
+def main():
+    tamanho = dificuldade()
+    campoAleatorio(tamanho)
+    mostrarMatriz(tamanho)
+
+#===================PROGRAMA PRINCIPAL===================#
+tamanho = []
+coluna = []
+matriz = []
+main()
