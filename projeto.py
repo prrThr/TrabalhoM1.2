43c9de06842b60187056b1869d9ad1cc6b9c77de
prrThr <arthur.op9@gmail.com>
2022-04-19 09:37:09 -0300

    updated matriz + padrao

--- projeto.py
+++ projeto.py
@@ -1,19 +1,6 @@
 import random
 
-def campoAleatorio(tamanho):
-    for l in range(tamanho):
-        linha = []
-        for c in range(tamanho):
-            num = random.randint(-1, 0)
-            linha.append(num)
-        matriz.append(linha)
-
-def mostrarMatriz(tamanho):
-    for i in range(tamanho):
-        for j in range(tamanho):
-            print(f'{matriz[i][j]:^3}', ' ', end='')
-        print('')       
-
+# ===== Escolhe uma das 3 dificuldades (apenas matrizes quadradas) ===== #
 def dificuldade():
     print('=====================================')
     print('ESCOLHA A DIFICULDADE: ')
@@ -25,6 +12,7 @@ def dificuldade():
         escolhaDificuldade = int(input())
         if escolhaDificuldade != 1 and escolhaDificuldade != 2 and escolhaDificuldade != 3:
             existe = False
+            print('Digite um valor dentro do intervalo 1-3')
         else:   
             existe = True
 
@@ -35,15 +23,51 @@ def dificuldade():
     else:
         return 15
 
+# ===== Gera campo aleatório a partir da dificuldade escolhida. ===== #
+def gerarCampo(t):
+    matriz = []
+    for l in range(t):
+        linha = []
+        for c in range(t):
+            num = random.randint(-1, 0)
+            linha.append(num)
+        matriz.append(linha)
+    return matriz
+
+# ===== Mostra a matriz (usando apenas para construir o programa) ===== #
+def mostrarMatriz(t,m):
+    for i in range(t):
+        for j in range(t):
+            print(f'{m[i][j]:^3}', ' ', end='')
+        print('')       
+
+# ===== Matriz padrão apenas com '-'
+def default(t):
+    padrao = []
+    for l in range(t):
+        linha = []
+        for c in range(t):
+            linha.append('-')
+        padrao.append(linha) 
+    return padrao
+
+#def QntdBombas():
+    
+
 
 
 def main():
     tamanho = dificuldade()
-    campoAleatorio(tamanho)
-    mostrarMatriz(tamanho)
+    matriz = gerarCampo(tamanho)
+    padrao = default(tamanho)
+    mostrarMatriz(tamanho, matriz)
+    print ('')
+    mostrarMatriz(tamanho,padrao)
+        
+        
+
+
 
 #===================PROGRAMA PRINCIPAL===================#
-tamanho = []
-coluna = []
-matriz = []
+
 main()
